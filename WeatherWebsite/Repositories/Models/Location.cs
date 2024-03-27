daac421349697318319534d74286b80e8aa99544
Denis <denis09112004@gmail.com>
2024-03-27 20:22:30 +0200

    add models in repo

---
+++ WeatherWebsite/Repositories/Models/Location.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherFrontend.DAL.Models;
+
+public class Location
+{
+    public int LocationId { get; set; }
+    public string Name { get; set; }
+    public int UserId { get; set; }
+    public float Latitude { get; set; }
+    public float Longtitude { get; set; }
+}
---
+++ WeatherWebsite/Repositories/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherFrontend.DAL.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string RepeatedPassword { get; set; }
+    }
+}
