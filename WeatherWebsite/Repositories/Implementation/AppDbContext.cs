26af84da703926c671ae31276735b04279d26430
Denis <denis09112004@gmail.com>
2024-03-27 20:23:38 +0200

    dbcontext

---
+++ WeatherWebsite/Repositories/Implementation/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.SqlClient;
+
+namespace WeatherFrontend.DAL.Implementation
+{
+    public class AppDbContext
+    {
+        public static SqlConnection CreateConnection()
+        {
+            return new SqlConnection("Data Source=.;Initial Catalog=Weather;Integrated Security=true;");
+        }
+    }
+}
