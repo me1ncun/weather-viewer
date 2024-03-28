f6dfac70c318afa6940f8789e4f0324aaab66785
Denis <denis09112004@gmail.com>
2024-03-28 20:05:03 +0200

    create dto

---
+++ WeatherWebsite/DTO/Base/WeatherForecastResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherBackend.DTO.Base;
+
+public class WeatherForecastResponse
+{
+    public string Cod { get; set; }
+    public int Message { get; set; }
+    public int Cnt { get; set; }
+    public List<ForecastHourlyDTO> List { get; set; }
+}
