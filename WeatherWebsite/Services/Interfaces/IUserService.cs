7d6e0eede1d49eb5e07a02d9663f35de718fd4e9
Denis <denis09112004@gmail.com>
2024-03-30 15:39:23 +0200

    delete unused method in IUserService

--- WeatherWebsite/Services/Interfaces/IUserService.cs
+++ WeatherWebsite/Services/Interfaces/IUserService.cs
@@ -7,7 +7,6 @@ namespace WeatherFrontend.BL.Interfaces
     {
         public int? Authenticate(string login, string password);
         public User GetUserById(int id);
-        public string Encrypt(string password);
         public void Register(string login, string password);
         public User GetUser(string login, string password);
         public int GetUsersId(string login, string password);
